f74914698194b2dcf31999dd703383c73313ea57
unknown <nicolascguedert@gmail.com>
2022-11-30 19:52:31 -0300

    download audio track do video em mp3

--- videos.py
+++ videos.py
@@ -1,6 +1,8 @@
 #pyp install pytube
 from pytube import YouTube
-import os 
+import os
+import re
+import moviepy.editor as mp
 
 class Video():
     def __init__(self, link):
@@ -9,5 +11,12 @@ class Video():
 
 
     def downloadAudio(self):
-        audiodecria = self.objVideo.streams.filter(only_audio = True).filter(audio_codec= 'mp3').first()
-        out_file = audiodecria.download("download/audio")
+        self.objVideo.streams.filter(only_audio = True).first().download('download/Audio')
+        path = "C:/Users/Nicolas Constante G/Documents/ProjetoFinal/download/audio"
+        for file in os.listdir(path):                  #For para percorrer dentro da pasta passada anteriormente
+            if re.search('mp4', file):                 #If verificando se o arquivo e .MP4                    
+                mp4_path = os.path.join (path, file)  #Cria uma variavel para armazenar o arquivo .MP4
+                mp3_path = os.path.join (path, os.path.splitext(file)[0] + '.mp3') # Variavel que cria o nome do arquivo e adiciona .MP3 ao final
+                new_file = mp.AudioFileClip(mp4_path)  #Cria o arquivo de áudio (.MP3)
+                new_file.write_audiofile(mp3_path)     #Renomeia o arquivo, setando o nome criado anteriormente
+                os.remove(mp4_path)                    #Remove o arquivo .MP4

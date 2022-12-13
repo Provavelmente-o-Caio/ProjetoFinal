e62ed4317cff0e87ee628bc68b3d698c177ae2a4
Provavelmente-o-Caio <caiofecardoso@hotmail.com>
2022-12-12 21:42:25 -0300

    merge com a branch do constante

--- menu.py
+++ menu.py
@@ -2,15 +2,10 @@ from videos import *
 #pip install PySimpleGUI
 import PySimpleGUI as sg
 
-
-opt = ['mp3', 'mp4']
-layout = [
-        [sg.Text('Url de origem:', size=(15,1)), sg.InputText(size=(50,1))], #primeira linha
-        [sg.Text('Pasta de destino:', size=(15,1)), sg.InputText(size=(40,1)), sg.FolderBrowse(size=(5,1))], #segunda linha
-        [sg.Text('Perfil de Saída:', size=(15,1)), sg.Text('Nome do arquivo', size=(40,1)),sg.DD(opt, size=(5,1))], #terceira linha
-        [sg.Button('Cancel', size=(5,1))]
-]
-janela = sg.Window("C & C DOWNLOADER", layout)
+layout = [[sg.Text('Url de origem:'), sg.InputText()], #primeira linha
+        [sg.Text('Pasta de destino:'), sg.InputText(), sg.FolderBrowse()], #segunda linha
+        [sg.Button('Download Video'), sg.Button('Download Audio'), sg.Button('Cancel')]] #terceira linha
+janela = sg.Window("MEDIA DOWNLOADER", layout)
 
      
 while True:
